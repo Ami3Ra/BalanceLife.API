3568a5feacfd1b8a547c4fd4700ae6c089c8bc82
Amira-Amr-25 <amiraamr445@gmail.com>
2026-02-08 12:30:49 +0200

    Product Module Configuration -Update Db

---
+++ BalanceLife.Domain/Entities/MealModule/Meal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BalanceLife.Domain.Entities.MealModule
+{
+    public class Meal:BaseEntity<int>
+    {
+        public string Name { get; set; } = default!;
+        public string PictureUrl { get; set; } = default!;
+        public int Calories { get; set; }
+        public decimal Protein { get; set; }
+        public decimal Carbs { get; set; }
+        public decimal Fats { get; set; }
+        public bool IsRecommended { get; set; }
+
+    }
+}
---
+++ BalanceLife.Persistence/Data/DbContexts/BalanceDbContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using BalanceLife.Domain.Entities.MealModule;
+using Microsoft.EntityFrameworkCore;
+
+namespace BalanceLife.Persistence.Data.DbContexts
+{
+    public class BalanceDbContext:DbContext
+    {
+        public BalanceDbContext(DbContextOptions<BalanceDbContext> options):base(options)
+        {
+           
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+
+        public DbSet<Meal> Meals { get; set; }
+        public DbSet<Restaurant> Restaurants { get; set; }
+
+    }
+}
